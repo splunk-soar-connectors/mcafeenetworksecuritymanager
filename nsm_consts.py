eb94a5584ee7a66bf91bfd037f4c7188d4b30931
Brian Earle <brian@Brians-MacBook-Pro.local>
2017-12-01 11:26:27 -0800

    Initial commit

---
+++ nsm_consts.py
@@ -0,0 +1,13 @@
+NSM_IP = "nsm_ip"
+NSM_USER = "nsm_user"
+NSM_PW = "nsm_pw"
+NSM_SENSOR = "sensor_id"
+NSM_BLOCK_IP = "ip"
+NSM_DURATION = "duration"
+
+NSM_ERR_QUERY = "NSM query failed"
+NSM_SUCC_QUERY = "NSM query successful"
+NSM_ERR_QUERY_RETURNED_NO_DATA = "NSM did not return any information"
+NSM_ERR_SERVER_CONNECTION = "Connection to server failed"
+NSM_ERR_CONNECTIVITY_TEST = "Connectivity test failed"
+NSM_SUCC_CONNECTIVITY_TEST = "Connectivity test passed"
